7ee0e57906bf9cccc45eca1e53255e81c35ce656
abcd <babcd>
2020-07-08 20:50:13 +0700

    add db, git ignore

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ var auth = require('../controller/auth.middleware');
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const axios = require('axios');
 
 //Fetch
 const fetch = require('node-fetch');
@@ -30,130 +31,92 @@ var mqtt = require('mqtt');
 const { log } = require('debug');
 const { json } = require('express');
 ip = 'mqtt://52.188.19.7:1883';
-var client = mqtt.connect(ip)
+var client = mqtt.connect(ip);
 //mqtt://13.76.250.158:1883
 //mqtt://52.188.19.7:1883
+//sv truong
+//mqtt://52.187.125.59:1883
 var mess;
 //Sub sensor start
 var topicSensor = 'Topic/TempHumi';
 
-client.on('message', (topicSensor, message) => {
-  //  message = message.toString();
+// async function getLimitTempt(daa){
+//  var selectdblimit = `SELECT * FROM iot.LIMITTEMPT where id = (select Max(id) from iot.LIMITTEMPT);  `;
+//  a = await con.query(selectdblimit);
+// console.log(a.data);
+
+// }
+
+var limit;
+const ex = async () => {
+
+  limit = await axios.get('http://localhost:3000/apigioihannhietdo');
+
+
+  console.log(limit.data);
+  return limit;
+}
+
+
+ client.on('message', (topicSensor, message) => {
   message = JSON.parse(message)[0];
+  let tempt = message;
 
-  if (message.device_id == 'TempHumi ') {
-
-    if (message.values[0] > 30) {
-
+   async function a(message){
      
-      console.log(message.values[0]);
+    limit = await axios.get('http://localhost:3000/apigioihannhietdo');
 
-      //insert nhiet do
-
-      var insert = `INSERT INTO CamBien (device,nhietdo,doam,thoigian) values ('${message.device_id}','${message.values[0]}','${message.values[1]}','${d}') `;
-
-      con.query(insert, function (err, result, fields) {
-        if (err) { throw err } else {
- //  On motor
-      // MQTT publisher
-      var client = mqtt.connect(ip)
-      var topic = 'Topic/Speaker'
-      // var message = 'Hello tempt! 1'
-      var message = [{ "device_id": "Speaker", "values": ["1", "80"] }]
-      var mess = JSON.stringify(message);
-      client.on('connect', () => {
-        console.log('Message on motor sent!',)
-
-        client.publish(topic, mess);
-      })
-        }
-      })
-    } else if (message.values[0] < 20) {
-     
-
-      //insert nhiet do
-
-      var insert = `INSERT INTO CamBien (device,nhietdo,doam,thoigian) values ('${message.device_id}','${message.values[0]}','${message.values[1]}','${d}') `;
-
-      con.query(insert, function (err, result, fields) {
-        if (err) { throw err } else {
-           //  Off motor
-      // MQTT publisher
-      var client = mqtt.connect(ip)
-      var topic = 'Topic/Speaker'
-      // var message = 'Hello tempt! 1'
-      var message = [{ "device_id": "Speaker", "values": ["0", "80"] }]
-      var mess = JSON.stringify(message);
-      client.on('connect', () => {
-        console.log('Message off motor sent!',)
-
-        client.publish(topic, mess);
-      })
-        }
-      })
-    } else {
-      //insert nhiet do
-
-      var insert = `INSERT INTO CamBien (device,nhietdo,doam,thoigian) values ('${message.device_id}','${message.values[0]}','${message.values[1]}','${d}') `;
-console.log('ko lam gi ca');
-
-      con.query(insert, function (err, result, fields) {
-        if (err) { throw err }
-      })
-    }
+    
 
 
-    // mess = message;
-    // var selectdblimit = `SELECT * FROM iot.LIMITTEMPT where id = (select Max(id) from iot.Motor); `;
-    // con.query(selectdblimit, function (err, result, obj) {
-    //   if (err) {
-    //     console.log(err)
-    //   } else {
+if (message.device_id == 'TempHumi ') {
+  
+  
+        //them vao db
+  var insert = `INSERT INTO CamBien (device,nhietdo,doam,thoigian) values ('${message.device_id}','${message.values[0]}','${message.values[1]}','${d}') `;
+  var run =  con.query(insert);
 
-    //     if (30 >= result.gioihantren) {
+  
+  if(message.values[0] > limit.data[0].gioihantren){
+    //On motor
+// MQTT publisher
+var client = mqtt.connect(ip)
+var topic = 'Topic/Speaker'
+// var message = 'Hello tempt! 1'
+var message = [{ "device_id": "Speaker", "values": ["1", "80"] }]
+var mess = JSON.stringify(message);
+client.on('connect', () => {
 
-    //       //Mo motor
-    //       // MQTT publisher
-    //       var client = mqtt.connect(ip)
-    //       var topic = 'Topic/Speaker'
-    //       // var message = 'Hello tempt! 1'
-    //       var message = [{ "device_id": "Speaker", "values": ["1", "80"] }]
-    //       var mess = JSON.stringify(message);
-    //       client.on('connect', () => {
-
-    //         client.publish(topic, mess);
-    //         //Luu khoi dong motor
-
-    //       })
-
-    //     } else if (20 <= result.giohanduoi) {
-
-    //        //Dong motor
-    //       // MQTT publisher
-    //       var client = mqtt.connect(ip)
-    //       var topic = 'Topic/Speaker'
-    //       // var message = 'Hello tempt! 1'
-    //       var message = [{ "device_id": "Speaker", "values": ["0", "80"] }]
-    //       var mess = JSON.stringify(message);
-    //       client.on('connect', () => {
-
-    //         client.publish(topic, mess);
-    //         //Luu khoi dong motor
-
-    //       })
-    //     } else {
+client.publish(topic, mess);
+console.log('Message on motor sent!',)
 
 
+})
+  } else if(message.values[0] < limit.data[0].gioihanduoi){
+      //OFF motor
+// MQTT publisher
+var client = mqtt.connect(ip)
+var topic = 'Topic/Speaker'
+// var message = 'Hello tempt! 1'
+var message = [{ "device_id": "Speaker", "values": ["0", "80"] }]
+var mess = JSON.stringify(message);
+client.on('connect', () => {
 
-    //     }
-
-    //   }
-    // })
+client.publish(topic, mess);
+console.log('Message off motor sent!',)
 
 
-  }
+  })
+}
+  
+  
 
 
+  // var selectdblimit = `SELECT * FROM iot.LIMITTEMPT where id = (select Max(id) from iot.Motor); `;
+ 
+}
+   }
+  a(tempt);
 
 })
 
@@ -161,6 +124,9 @@ client.on('connect', () => {
   client.subscribe(topicSensor)
 })
 //Sub sensor End
+
+
+
 //Sub motor start
 
 var topicMotor = 'Topic/Speaker';
@@ -215,6 +181,17 @@ router.get('/apinhietdo', function (req, res, next) {
 
   });
 });
+/* API data gioi han nhiet do. */
+router.get('/apigioihannhietdo', function (req, res, next) {
+  // var select = `SELECT * FROM CamBien where idCamBien `;
+  var select = `
+  SELECT * FROM iot.LIMITTEMPT where id = (select max(id) from iot.LIMITTEMPT);`
+  con.query(select, function (err, result, fields) {
+    if (err) throw err;
+    res.send(result);
+
+  });
+});
 //Bieu do
 router.get('/bieudonhietdo', function (req, res, next) {
 
@@ -244,12 +221,11 @@ router.get('/trangthaimotor', function (req, res, next) {
   }
   var data;
   //lay gioi han nhiet do
-  var selectdblimit = `SELECT * FROM iot.LIMITTEMPT where id = (select Max(id) from iot.Motor); `;
+  var selectdblimit = `SELECT * FROM iot.LIMITTEMPT where id = (select max(id) from iot.LIMITTEMPT); `;
   con.query(selectdblimit, function (err, result, obj) {
     if (err) {
       console.log(err)
     } else {
-      console.log(result);
 
       obj.gioihantren = result[0].gioihantren;
       obj.gioihanduoi = result[0].gioihanduoi;
@@ -287,7 +263,6 @@ router.post('/tatmotor', function (req, res, next) {
   // MQTT publisher
   var client = mqtt.connect(ip)
   var topic = 'Topic/Speaker'
-  // var message = 'Hello tempt! 1'
   var message = [{ "device_id": "Speaker", "values": ["0", "80"] }]
   var mess = JSON.stringify(message);
   client.on('connect', () => {
@@ -312,7 +287,7 @@ router.post('/momotor', function (req, res, next) {
   client.on('connect', () => {
 
     client.publish(topic, mess);
-    console.log('Message off motor sent!',)
+    console.log('Message on motor sent!',)
     res.redirect('/trangthaimotor');
 
   })
